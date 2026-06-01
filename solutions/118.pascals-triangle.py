1456718a7c9bc42bf752acd2fe6892d0507825b6
kairoswong <kairoswong@westlake.edu.cn>
2026-06-01 16:25:26 +0800

    feat: solve 118.pascals-triangle

---
+++ solutions/118.pascals-triangle.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=118 lang=python3
+#
+# [118] Pascal's Triangle
+#
+
+# @lc code=start
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+
+        return triangle
+# @lc code=end
+
